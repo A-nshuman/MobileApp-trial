406b59f323428c2fedd3b8860a2369a713aa215f
A-nshuman <abtbbruv@gmail.com>
2024-05-16 13:32:10 +0300

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import { Slot, Stack } from 'expo-router'
+import React from 'react'
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+    </Stack>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#202020",
+        secondary: "#2b2b2b",
+        primary: "#e6f2f2",
+        accent: "#9bdbde",
+        'dull-blue': '#84d5ed',
+        'bright-blue': '#2b83e2',
+        'dull-red': '#ff6666',
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
