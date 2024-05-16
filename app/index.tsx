1b453d061c2b8499f2589427fd61a251ebf5ab62
A-nshuman <abtbbruv@gmail.com>
2024-05-16 13:44:03 +0300

    second commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import { View, Text, Button } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { Link } from 'expo-router'
 
 const index = () => {
 
@@ -8,7 +7,6 @@ const index = () => {
     <View className="flex-1 flex justify-center items-center bg-background">
       <Text className='text-primary text-3xl'>Welcome Anshuman</Text>
       <StatusBar style='auto' />
-      <Link href={'/profile'} className='text-blue-500'>Go to Profile</Link>
     </View>
   );
 
